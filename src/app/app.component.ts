067bd85326c0208b33bea6bd868c1b49097b4f50
ToniIvankovic <toni.ivankovic@gmail.com>
2022-07-10 12:14:41 +0200

    Initial cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-	title = 'infinum-academy-app';
-}
+export class AppComponent {}
