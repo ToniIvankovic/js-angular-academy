0cb588d882779322246f2ce6d3aec8498d585237
ToniIvankovic <toni.ivankovic@gmail.com>
2022-07-10 15:27:35 +0200

    Added all modules, project structure fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ShowsContainerComponentModule } from './shows-container-component/shows-container/shows-container-component.module';
+import { ShowsContainerComponentModule } from './components/shows-container/shows-container-component.module';
 
 @NgModule({
 	declarations: [AppComponent],
---
+++ src/app/components/show-card/show-card.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShowCardComponent } from './show-card.component';
+
+@NgModule({
+	declarations: [ShowCardComponent],
+	imports: [CommonModule],
+	exports: [ShowCardComponent],
+})
+export class ShowCardModule {}
