671fa126ebfa4414a83c9e05316e1e43a7f2a3cf
ToniIvankovic <toni.ivankovic@gmail.com>
2022-07-10 16:10:34 +0200

    Basic card functionality

--- src/app/components/show-card/show-card.component.ts
+++ src/app/components/show-card/show-card.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-show-card',
 	templateUrl: './show-card.component.html',
 	styleUrls: ['./show-card.component.scss'],
 })
-export class ShowCardComponent {}
+export class ShowCardComponent {
+	@Input() public title: string = '';
+	@Input() public averageRating: number | null = null;
+	@Input() public imageUrl: string | null = null;
+}
