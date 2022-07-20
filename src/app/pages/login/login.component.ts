71feeff8e678821c7669936ca6489f29c8bc8a17
ToniIvankovic <toni.ivankovic@gmail.com>
2022-07-20 21:58:02 +0200

    Added login errors

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -16,10 +16,52 @@ export class LoginComponent {
 
 	public form: FormGroup;
 
-	public onLoginClick(event: Event) {
-		event.preventDefault();
-		console.log(this.form.controls['email'].errors);
+	public get emailErrors() {
+		return this.form.controls['email'].errors;
+	}
 
-		console.log(event);
+	public get passwordErrors() {
+		return this.form.controls['password'].errors;
+	}
+
+	public get formDirty() {
+		return this.form.controls['email'].dirty && this.form.controls['password'].dirty;
+	}
+
+	public errors: string[] = [];
+
+	public onLoginClick(event: Event) {
+		this.errors = [];
+		event.preventDefault();
+
+		if (this.emailErrors || this.passwordErrors) {
+			// this.errors = of(["A"]).pipe(delay(500));
+			this.errors = [];
+			if (this.emailErrors) {
+				if (this.emailErrors['email']) {
+					this.errors.push('Invalid email address format');
+				} else if (this.emailErrors['required']) {
+					this.errors.push('Email address required');
+				} else {
+					this.errors.push('Email address problem');
+				}
+			}
+			if (this.passwordErrors) {
+				if (this.passwordErrors['minlength']) {
+					this.errors.push('Password is too short - minimum length is 8 characters');
+				} else if (this.passwordErrors['required']) {
+					this.errors.push('Password is required');
+				} else {
+					this.errors.push('Password problem');
+				}
+			}
+			console.log(this.emailErrors);
+			console.log(this.passwordErrors);
+			console.log(!!this.errors);
+			console.log(this.errors);
+			return;
+		}
+
+		//TODO acutal login
 	}
 }
