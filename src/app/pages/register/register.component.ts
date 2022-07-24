e39506ecb6a3f95d9d7e81805220cec8ad57958d
ToniIvankovic <toni.ivankovic@gmail.com>
2022-07-24 13:37:11 +0200

    Auth layout and register redesign + pwd confirmation validation

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -19,6 +19,7 @@ export class RegisterComponent {
 		});
 	}
 
+	public imgSrc = '../../../assets/logo.svg';
 	public form: FormGroup;
 
 	public get emailErrors() {
@@ -28,16 +29,14 @@ export class RegisterComponent {
 	public get passwordErrors() {
 		return this.form.controls['password'].errors;
 	}
-
-	public errors: string[] = [];
+	public get password2Errors() {
+		return this.form.controls['password2'].errors;
+	}
 
 	public onSignUpClick(event: Event) {
-		this.errors = [];
 		event.preventDefault();
 
-		this.checkForErrors();
-
-		if (this.errors.length !== 0) {
+		if (this.form.invalid) {
 			return;
 		}
 
@@ -49,40 +48,28 @@ export class RegisterComponent {
 			})
 			.pipe(catchError(this.handleErrorResponse.bind(this)))
 			.subscribe((user) => {
-				//Add error handling
 				console.log(user);
 				this.router.navigate(['/']);
 			});
 	}
 
-	private checkForErrors() {
-		if (this.emailErrors || this.passwordErrors) {
-			// this.errors = of(["A"]).pipe(delay(500));
-			if (this.emailErrors) {
-				if (this.emailErrors['email']) {
-					this.errors.push('Invalid email address format');
-				} else if (this.emailErrors['required']) {
-					this.errors.push('Email address required');
-				} else {
-					this.errors.push('Email address problem');
-				}
-			}
-			if (this.passwordErrors) {
-				if (this.passwordErrors['minlength']) {
-					this.errors.push('Password is too short - minimum length is 8 characters');
-				} else if (this.passwordErrors['required']) {
-					this.errors.push('Password is required');
-				} else {
-					this.errors.push('Password problem');
-				}
-			}
-		} else if (this.form.controls['password'].value !== this.form.controls['password2'].value) {
-			this.errors.push("Passwords don't match");
+	public passBlur() {
+		if (this.form.controls['password'].value !== this.form.controls['password2'].value) {
+			this.form.controls['password2'].setErrors({
+				match: "Passwords don't match",
+			});
 		}
 	}
 
+	public inputChanged() {
+		this.form.controls['password2'].setErrors(null);
+	}
+
 	private handleErrorResponse(error: HttpErrorResponse) {
-		this.errors = error.error.errors;
+		console.log(error.error.errors);
+		this.form.controls['email'].setErrors({
+			existing: true,
+		});
 		return EMPTY;
 	}
 }
--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -6,10 +6,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
 	declarations: [RegisterComponent],
 	exports: [RegisterComponent],
-	imports: [CommonModule, MatCardModule, MatInputModule, RouterModule, MatButtonModule, ReactiveFormsModule],
+	imports: [
+		CommonModule,
+		MatCardModule,
+		MatInputModule,
+		RouterModule,
+		MatButtonModule,
+		ReactiveFormsModule,
+		MatIconModule,
+	],
 })
 export class RegisterModule {}
