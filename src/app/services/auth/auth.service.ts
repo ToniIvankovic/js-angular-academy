fa4d89c47a3bb5f41d3c37e14b4f8e1c8d2891d3
ToniIvankovic <toni.ivankovic@gmail.com>
2022-07-26 15:27:22 +0200

    Saving token to storage on login

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { IAuthRegisterUser } from './auth-register-user';
-import { HttpClient } from '@angular/common/http';
-import { catchError, EMPTY, Observable, of } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { catchError, EMPTY, map, Observable, of, tap } from 'rxjs';
 import { IUser } from './user.interface';
 import { IAuthLoginUser } from './auth-login-user';
 
@@ -15,8 +15,20 @@ export class AuthService {
 		return this.http.post<IUser>('https://tv-shows.infinum.academy/users', formData);
 	}
 	public login(formData: IAuthLoginUser): Observable<IUser> {
-		return this.http.post<IUser>('https://tv-shows.infinum.academy/users/sign_in', formData);
+		return this.http
+			.post<any>('https://tv-shows.infinum.academy/users/sign_in', formData, { observe: 'response' })
+			.pipe(
+				tap((response) => {
+					localStorage.setItem('acces-token', response.headers.get('access-token') || '');
+					localStorage.setItem('client', response.headers.get('client') || '');
+					localStorage.setItem('uid', response.headers.get('uid') || '');
+				}),
+				map((response) => {
+					return response.body['user'];
+				}),
+			);
 	}
+
 	public getCurrentUser(): Observable<IUser | null> {
 		return this.http.get<IUser>('https://tv-shows.infinum.academy/users/me').pipe(
 			catchError(() => {
