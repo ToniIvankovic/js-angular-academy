bc4cf13af7c865b2eb195df4601fae1d9a7143c7
ToniIvankovic <toni.ivankovic@gmail.com>
2022-07-30 18:13:23 +0200

    Added the capitalization pipe

--- src/app/components/show-card/show-card.module.ts
+++ src/app/components/show-card/show-card.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ShowCardComponent } from './show-card.component';
 import { MatCardModule } from '@angular/material/card';
 import { RatingModule } from '../rating/rating.module';
+import { CapitalizeModule } from 'src/app/pipes/capitalize/capitalize.module';
 
 @NgModule({
 	declarations: [ShowCardComponent],
-	imports: [CommonModule, MatCardModule, RatingModule],
+	imports: [CommonModule, MatCardModule, RatingModule, CapitalizeModule],
 	exports: [ShowCardComponent],
 })
 export class ShowCardModule {}
